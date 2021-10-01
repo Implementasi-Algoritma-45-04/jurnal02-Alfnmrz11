1cb5ccde2dbfd3a12487f3cb9583238755f6b576
alfian mohammad rizki <alfianmrizki123@gmail.com>
2021-10-01 14:53:02 +0700

    solusi jurnal 02

---
+++ jurnal02.java
@@ -0,0 +1,8 @@
+public class jurnal02 {
+
+	public static void main(String[] args) {
+		System.out.println("NIM 	: 6706213005");
+		System.out.println("Nama 	: Alfian Mohammad Rizki");
+		System.out.println("Kelas 	: D3IF-45-04");
+	}
+}
